2241d17ab334b83068831d02f8feb45ded9bba08
Robert Booth <boothr16@gmail.com>
2021-08-10 15:46:46 -0400

    populateUI() fetches data from local storage and displays it on page reload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value; // + sign casts ticketPrice to a Number/Integer
 
 // Save selected movie index and price
@@ -23,18 +25,37 @@ function updateSelectedCount() {
     const seatsIndex = [...selectedSeats].map(seat =>
         [...seats].indexOf(seat)); // ... copies elements from selectedSeats to seatsIndex. map() returns a new array
 
-    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex)); // convert array to string
 
-    const selectedSeatsCount = selectedSeats.length;
+    const selectedSeatsCount = selectedSeats.length; // number of selected seats
 
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+    count.innerText = selectedSeatsCount; // update text to new count
+    total.innerText = selectedSeatsCount * ticketPrice; // update text to new total price
+}
+
+// Get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')); // convert string to array
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
 // Movie select event
-movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    setMovieData(e.target.selectedIndex, e.target.value);
+movieSelect.addEventListener('change', e => { // executes when changing movie choice
+    ticketPrice = +e.target.value; // ticket price updates to match movie
+    setMovieData(e.target.selectedIndex, e.target.value); // saves menu index and price to local storage
     updateSelectedCount();
 })
 
@@ -46,3 +67,6 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 })
+
+// Initial count and total set
+updateSelectedCount();
